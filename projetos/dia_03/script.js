8c3a44d1861bb231172848126912eec051844ce5
Gabriel Chollet <62822641+Gabrielchollet@users.noreply.github.com>
2021-04-11 00:12:33 -0300

    Mentalista
    Fase inicial do desenvolvimento.
    OBS: tornar o css responsivo.

--- projetos/dia_03/script.js
+++ projetos/dia_03/script.js
@@ -1,53 +1,60 @@
 var h2 = document.querySelector('h2');
 var h3 = document.querySelector('h3');
-var x = document.getElementById("div2");
+var bnt = document.querySelector("div2");
 
 function jogar() {
   alert("Saudações, caro viajante, eu sou o Guardião... ");
   var resposta = prompt("Está a procura da Torre Sem-Fim... Sim ou Não?");
   if (resposta == "Sim") {
-    var idade = parseInt(prompt("Diga-me viajante qual sua idade e se acertar o desafio lhe levarei até lá"));
+    var idade = parseInt(prompt("Diga-me viajante a quantos anos está na Terra e se vencer o desafio lhe levarei até seu destino"));
     document.getElementById('btnJogar').disabled = true;
   } else {
     jogar();
   }
-  h2.textContent = "Advinhe o numero que estou pensando entre 0 e " + idade;
-  
-  sortearNumero(idade);
-}
-
-
-function sortearNumero(num) {
-  var numeroSecreto = parseInt(Math.random() * num + 1);
+  h2.textContent = "Advinhe o número que estou pensando entre 0 e " + idade;
+    
   var vidas = parseInt(Math.random() * 5 + 1);  
   h3.textContent = "Vidas: " + vidas;
-  
-  mecanismo(numeroSecreto, vidas)
+  mecanismo(sortearNumero(idade), vidas)
 }
 
-function mecanismo(intX, intY) {
-    x.style.dispay = "block";
-    var chute = parseInt(document.getElementById('numero').value);
-    while (intY > 0) {
-        if (chute == intX) {
-            h2.textContent = "Você acertou ! agora corra";
-            h3.textContent = '';
-            break;
+
+function sortearNumero(age) {
+  var numeroSecreto = parseInt(Math.random() * age + 1);
+} 
+
+function mecanismo(numeroSorteado, life) {
+  bnt = `<div class"bntEnviar" id="div2" style="display: block">
+      <input type="number" id="numero">
+      <button onclick="tentativa()">Enviar</button>
+    </div>`
+  var chute = parseInt(document.getElementById('numero').value);
+  while (life > 0)
+    {
+      if (chute == numeroSorteado)
+        {
+          h2.textContent = "Incrível, você acertou ! agora vamos";
+          h3.textContent = '';
+          break;
         }
-        else if (chute < intX) {
-            h2.textContent = "Errado, darei-lhe uma dica: é maior.";
-            intY = intY - 1;
-            h3.textContent = "Tentativas restantes: " + intY;
-            break;
+      else if (chute < numeroSorteado)
+        {
+          h2.textContent = "Errado, darei-lhe uma dica: é maior.";
+          life--;
+          h3.textContent = "Tentativas restantes: " + life;
+          break;
         }
-        else {
-            h2.textContent = "Errado, darei-lhe uma dica: é menor.";
-            intY = intY - 1;
-            h3.textContent = "Tentativas restantes: " + intY;
-            break;
-        } 
+      else if (chute > numeroSorteado)
+        {
+          h2.textContent = "Errado, darei-lhe uma dica: é menor.";
+          life--;
+          h3.textContent = "Tentativas restantes: " + life;
+          break;
+        }
+      
     }
-    if(intY == 0) {
-        h2.textContent = "É uma pena, volte quando estiver pronto."
-    }
+  if(life == 0)
+      {
+          h2.textContent = "É uma pena, volte quando estiver pronto."
+      } 
 }  
